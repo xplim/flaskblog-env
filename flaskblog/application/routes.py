620433c2432f93280f68c3d55abbc119c80bf5b3
xplim <75422036+xplim@users.noreply.github.com>
2021-08-22 20:49:19 +0800

    Add 'post' page to show details of single post

--- flaskblog/application/routes.py
+++ flaskblog/application/routes.py
@@ -125,3 +125,14 @@ def new_post():
         flash("Your post has been updated!", "success")
         return redirect(url_for("home"))
     return render_template("create_post.html", title="New Post", form=form)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template(
+        "post.html",
+        title=post.title,
+        post=post,
+        url_for_author_image=url_for_author_image,
+    )
